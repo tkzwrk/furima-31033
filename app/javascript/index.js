4117a0cea7ff1ee2a4b38e4b4114855435fd091e
tkzwrk <72016605+tkzwrk@users.noreply.github.com>
2020-10-18 18:00:04 +0900

    createアクションの変更とjsファイルの修正

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -2,10 +2,10 @@ window.addEventListener("load", function() {
   let price = document.getElementById("item-price");
   price.addEventListener("input", function() {
     let item_price = parseInt(price.value);
-    let tax1 = document.getElementById("add-tax-price");
-    tax1.innerHTML = (item_price * 0.1);
-    let sum1 = document.getElementById("profit");
-    sum1.innerHTML = (item_price - (item_price * 0.1));
+    let tax = document.getElementById("add-tax-price");
+    tax.innerHTML = (item_price * 0.1);
+    let sum = document.getElementById("profit");
+    sum.innerHTML = (item_price - (item_price * 0.1));
   });
 });
 
