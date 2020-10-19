695b0cd3b82a6d3f997b819b54c5728376d82004
tkzwrk <72016605+tkzwrk@users.noreply.github.com>
2020-10-19 12:27:10 +0900

    ログアウト状態でも商品一覧が表示できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show, :search]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
